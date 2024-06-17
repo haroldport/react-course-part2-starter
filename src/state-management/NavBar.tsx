6c52ccd362224fb41a7b29e242a554448d1a1a93
Harold Portocarrero <harold.port@gmail.com>
2024-06-17 17:53:01 -0500

    Prevent unnecessary renders with selectors in Zustand

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,11 +1,10 @@
-import { useContext } from "react";
 import LoginStatus from "./auth/LoginStatus";
 import useCounterStore from "./counter/store";
-import TasksContext from "./tasks/tasksContext";
 
 const NavBar = () => {
-  const { tasks } = useContext(TasksContext);
-  const { counter } = useCounterStore();
+  const counter = useCounterStore((s) => s.counter);
+
+  console.log("Render NavBar");
 
   return (
     <nav className="navbar d-flex justify-content-between">
