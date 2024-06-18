e05a6943a7b64df7d1697abe8058863fecfba805
Harold Portocarrero <harold.port@gmail.com>
2024-06-18 17:35:27 -0500

    Navigation

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import router from "./routing/routers";
+import router from "./routing/routes";
 
 const queryClient = new QueryClient();
 
